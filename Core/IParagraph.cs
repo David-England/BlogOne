db305e7c2e5e611e76217f8483d01eb416e095de
David England <david.england.de@googlemail.com>
2024-01-26 09:05:23 +0000

    Add most important entities

---
+++ Core/IParagraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IParagraph : IBlogElement
+    {
+        public string GetText();
+    }
+}
