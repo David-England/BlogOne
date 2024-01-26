e05ad24428793bd49e99bce2744b7b698082aa0c
David England <david.england.de@googlemail.com>
2024-01-26 09:18:34 +0000

    Make BlogCollection an interface

---
+++ Core/IBlogCollection.cs
@@ -0,0 +1,8 @@
+﻿namespace Core
+{
+    public interface IBlogCollection
+    {
+        public IEnumerable<IBlogElement> GetAllBlogs();
+        public IEnumerable<IEnumerable<IBlogElement>> GetNBlogs(int n);
+    }
+}
