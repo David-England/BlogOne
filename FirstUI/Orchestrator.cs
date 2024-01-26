3c795d4f7f3f5dd786925533e73cef01d4a041ac
David England <david.england.de@googlemail.com>
2024-01-26 19:31:18 +0000

    Load blogs

---
+++ FirstUI/Orchestrator.cs
@@ -0,0 +1,23 @@
+﻿using Core;
+using FlatFileStore;
+
+namespace FirstUI
+{
+	public static class Orchestrator
+	{
+		private static IBlogCollection _store = Store.Create("Blogs");
+
+		public static IEnumerable<IBlog> AllBlogs
+		{
+			get
+			{
+				return _store.GetAllBlogs();
+			}
+		}
+
+		public static int IndexOf(IBlog blog)
+		{
+			return AllBlogs.Reverse().ToList().IndexOf(blog);
+		}
+	}
+}
