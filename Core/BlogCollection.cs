8754b2595c00787cab7811a7f54343b340ef58fa
David England <david.england.de@googlemail.com>
2024-01-26 08:24:47 +0000

    Initialise repo

---
+++ Core/BlogCollection.cs
@@ -0,0 +1,15 @@
+﻿namespace Core
+{
+    public static class BlogCollection
+    {
+        public static void GetAllBlogs()
+        {
+            return;
+        }
+
+        public static void GetNBlogs(int n)
+        {
+            return;
+        }
+    }
+}
