4ecba17fb1ad87a0f749395a9cae772b68ece44e
David England <david.england.de@googlemail.com>
2024-01-26 09:57:14 +0000

    Flat file store
    Initial work only

---
+++ FlatFileStore/Store.cs
@@ -0,0 +1,34 @@
+﻿using Core;
+
+namespace FlatFileStore
+{
+    public class Store : IBlogCollection
+    {
+        private List<IBlog> _blogs = new List<IBlog>();
+
+        private Store(string folderPath)
+        {
+            DirectoryInfo folder = Directory.CreateDirectory(folderPath);
+
+            foreach (var item in folder.EnumerateFiles())
+            {
+                //
+            }
+        }
+
+        public Store Create(string folderPath)
+        {
+            return new Store(folderPath);
+        }
+
+        public IEnumerable<IBlog> GetAllBlogs() => _blogs;
+
+        public IEnumerable<IEnumerable<IBlog>> GetNBlogs(int n)
+        {
+            for (int i = 0; i < _blogs.Count; i += n)
+            {
+                yield return _blogs.Skip(i).Take(n);
+            }
+        }
+    }
+}
