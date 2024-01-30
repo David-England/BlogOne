2cacfe573624315ccabc86d712a803ad397ea2d9
David England <david.england.de@googlemail.com>
2024-01-30 11:37:31 +0000

    Add test ensuring GetAllBlogs() always hits disc

---
+++ TestFlatFileStore/RefreshBlogs.cs
@@ -0,0 +1,19 @@
+﻿namespace TestFlatFileStore
+{
+	public class RefreshBlogs
+	{
+		private string _blogPath = "WithOneBlog/blog.txt";
+
+		[Fact]
+		public void InstantiateStoreThenAdd()
+		{
+			new FileInfo(_blogPath).Delete();
+			Store store = Store.Create("WithOneBlog");
+
+			File.Create(_blogPath).Close();
+			File.WriteAllLines(_blogPath, ["01/01/2024 New blog", "My friends, it's a new day."]);
+
+			Assert.Single(store.GetAllBlogs());
+		}
+	}
+}
