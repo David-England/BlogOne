b95efa126f637d91f46cd98aee51214377e202f7
David England <david.england.de@googlemail.com>
2024-02-01 09:38:31 +0000

    Fix: the UI still cached the Store

--- FirstUI/Orchestrator.cs
+++ FirstUI/Orchestrator.cs
@@ -5,17 +5,23 @@ namespace FirstUI
 {
 	public static class Orchestrator
 	{
-		private static IBlogCollection _store = Store.Create("Blogs");
+		private static IBlogCollection LocalStore
+		{
+			get
+			{
+				return Store.Create("Blogs");
+			}
+		}
 
 		public static IDictionary<string, IBlog> AllBlogs
 		{
 			get
 			{
-				return _store.GetAllBlogs().OrderByDescending(kv => kv.Value.CreatedDate)
+				return LocalStore.GetAllBlogs().OrderByDescending(kv => kv.Value.CreatedDate)
 					.ToDictionary();
 			}
 		}
 
-		public static IBlog GetBlog(string key) => _store.GetBlog(key);
+		public static IBlog GetBlog(string key) => LocalStore.GetBlog(key);
 	}
 }
