8958e88903271551033528d97a2e325a35ee9109
David England <david.england.de@googlemail.com>
2024-02-01 11:06:11 +0000

    Inject blogs into UI

--- FirstUI/Program.cs
+++ FirstUI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<Core.IBlogCollection, FlatFileStore.Store>(
+    (prov) => FlatFileStore.Store.Create("Blogs"));
 
 var app = builder.Build();
 
