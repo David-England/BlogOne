571db0453add4bdbf4aba1905dd66584567d8f27
David England <david.england.de@googlemail.com>
2024-02-02 09:31:46 +0000

    Add test for fetching image directory

---
+++ TestFlatFileStore/FetchImageDirectory.cs
@@ -0,0 +1,14 @@
+﻿namespace TestFlatFileStore
+{
+	public class FetchImageDirectory
+	{
+		[Fact]
+		public void GetImageDirectory()
+		{
+			Store store = Store.Create("TestStore");
+
+			DirectoryInfo di = new DirectoryInfo(store.ImagesDirectoryPath);
+			Assert.Equal("TestStore\\pics", Path.Combine(di.Parent!.Name, di.Name));
+		}
+	}
+}
