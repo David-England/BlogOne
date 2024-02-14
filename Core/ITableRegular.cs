fbaa4f6eb5cad6d8f0f8477268c67eca14745c15
David England <david.england.de@googlemail.com>
2024-02-14 07:58:03 +0000

    Table logic interface

---
+++ Core/ITableRegular.cs
@@ -0,0 +1,11 @@
+﻿namespace Core
+{
+	public interface ITableRegular : IBlogElement
+	{
+		public int nRows { get; }
+		public int nCols { get; }
+		public string[] Headers { get; }
+		public string[,] Cells { get; }
+		public string Caption { get; }
+	}
+}
